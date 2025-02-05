a23d4e0c6a5b380ebcaf39c4bbe8732a8dbf4677
Gideon Kemboi <gedionkemboi18@gmail.com>
2025-02-05 18:22:38 +0300

    Add proper ui for the app and refactor script to work with the ui

--- script.js
+++ script.js
@@ -1,176 +1,119 @@
-function formatDateString(dateString) {
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+async function getWeather(place) {
+  const API_KEY = "42a9b5d54a1d47e3b2755237242401";
+  const url = `https://api.weatherapi.com/v1/forecast.json?q=${place}&days=4&key=${API_KEY}`;
 
-  const date = new Date(dateString);
+  try {
+    const response = await fetch(url);
 
-  const dayOfWeek = daysOfWeek[date.getDay()];
-  const month = months[date.getMonth()];
-  const day = date.getDate();
-  const year = date.getFullYear();
+    if (!response.ok) {
+      throw new Error(`Weather data not found for "${place}"`);
+    }
 
-  let hours = date.getHours();
-  const minutes = date.getMinutes();
+    const data = await response.json();
 
-  const amPm = hours >= 12 ? "PM" : "AM";
+    console.log(data);
 
-  hours = hours % 12 || 12;
+    let location = data.location.name;
+    let country = data.location.country;
+    let tempCelcius = Math.round(data.current.temp_c);
+    let tempFahrenheit = data.current.temp_f;
+    let description = data.current.condition.text;
+    let humidity = data.current.humidity;
+    let wind = data.current.wind_kph;
+    let feelsLike = data.current.feelslike_c;
 
-  const time = `${hours}:${minutes < 10 ? "0" : ""}${minutes} ${amPm}`;
-
-  const formattedDate = {
-    dayOfWeek,
-    month,
-    day,
-    year,
-    time,
-  };
-
-  return formattedDate;
-}
-
-async function fetchWeather(place) {
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?q=${place}&days=4&key=42a9b5d54a1d47e3b2755237242401`
-  );
-  const data = await response.json();
-
-  let location = data.location.name;
-  let country = data.location.country;
-  let formattedDate = formatDateString(data.location.localtime);
-  let date =
-    formattedDate.dayOfWeek +
-    ", " +
-    formattedDate.month +
-    " " +
-    formattedDate.day +
-    ", " +
-    formattedDate.year;
-  let time = formattedDate.time;
-  let tempCelcius = data.current.temp_c;
-  let tempFahrenheit = data.current.temp_f;
-  let description = data.current.condition.text;
-  let humidity = data.current.humidity;
-  let wind = data.current.wind_kph;
-  let feelsLike = data.current.feelslike_c;
-
-  let currentWeather = {
-    location,
-    country,
-    date,
-    time,
-    tempCelcius,
-    tempFahrenheit,
-    description,
-    humidity,
-    wind,
-    feelsLike,
-  };
-
-  let forecastDays = data.forecast.forecastday;
-  let forecast = { dayForecasts: [] };
-
-  for (let day of forecastDays.slice(1)) {
-    let dayForecast = {
-      date: day.date,
-      temp: day.day.maxtemp_c,
-      description: day.day.condition.text,
-      humidity: day.day.avghumidity,
-      wind: day.day.maxwind_kph,
+    let weather = {
+      location,
+      country,
+      tempCelcius,
+      tempFahrenheit,
+      description,
+      humidity,
+      wind,
+      feelsLike,
     };
-    forecast.dayForecasts.push(dayForecast);
+
+    return weather;
+  } catch (error) {
+    console.error(error.message);
+    alert(error.message);
+    return null;
   }
-
-  let weather = {
-    currentWeather,
-    forecast,
-  };
-
-  return weather;
 }
 
 function displayWeather(weather) {
-  let currentWeather = weather.currentWeather;
-  let forecast = weather.forecast;
+  if (!weather) return;
 
   let location = document.querySelector(".location");
-  let date = document.querySelector(".date");
   let description = document.querySelector(".description");
   let temperature = document.querySelector(".temperature");
   let humidity = document.querySelector(".humidity");
   let wind = document.querySelector(".wind");
   let feelsLike = document.querySelector(".feelsLike");
+  let weatherInfo = document.querySelector(".info");
 
-  location.innerHTML = `${currentWeather.location}, ${currentWeather.country}`;
-  date.innerHTML = `${currentWeather.date} ${currentWeather.time}`;
-  description.innerHTML = currentWeather.description;
-  temperature.innerHTML = `${currentWeather.tempCelcius}°C`;
-  humidity.innerHTML = `${currentWeather.humidity}%`;
-  wind.innerHTML = `${currentWeather.wind}km/h`;
-  feelsLike.innerHTML = `${currentWeather.feelsLike}°C`;
+  location.innerHTML = `${weather.location}, ${weather.country}`;
+  description.innerHTML = weather.description;
+  temperature.innerHTML = `${weather.tempCelcius}<span class="degree">°C</span>`;
 
-  let forecastDiv = document.querySelector(".forecast");
-  forecastDiv.innerHTML = "";
-
-  for (let day of forecast.dayForecasts) {
-    let dayDiv = document.createElement("div");
-    dayDiv.classList.add("day");
-    let dayOfWeek = formatDateString(day.date).dayOfWeek
-    dayDiv.innerHTML = `${dayOfWeek} <br /> ${day.temp}°C <br /> ${day.description}`;
-    forecastDiv.appendChild(dayDiv);
-  }
+  feelsLike.innerHTML = `FEELS LIKE: ${weather.feelsLike}<sup>°C</sup>`;
+  humidity.innerHTML = `HUMIDITY: ${weather.humidity}%`;
+  wind.innerHTML = `WIND: ${weather.wind}km/h`;
+  weatherInfo.classList.add("loaded");
 }
 
-fetchWeather("Nairobi").then((weather) => {
-  console.log(weather);
+getWeather("Nairobi").then((weather) => {
   displayWeather(weather);
 });
 
-(() => {
-  function search(location) {
-    fetchWeather(location)
-      .then((weather) => {
-        displayWeather(weather);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  let searchBar = document.querySelector("#searchInput");
-  searchInput.addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      submitSearch();
-    }
+// Search functionality
+function search(location) {
+  getWeather(location).then((weather) => {
+    displayWeather(weather);
   });
+}
 
-  function submitSearch() {
-    const query = searchInput.value.trim();
-    if (query !== "") {
-      search(query);
-      searchInput.value = "";
-    }
+let searchBar = document.querySelector("#searchInput");
+searchInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    submitSearch();
   }
-})();
+});
+
+function submitSearch() {
+  const query = searchInput.value.trim();
+  if (query !== "") {
+    search(query);
+    searchInput.value = "";
+  }
+}
+
+function changeBackground(weatherCondition) {
+  let backgroundImage = "";
+
+  switch (weatherCondition.toLowerCase()) {
+    case "clear":
+      backgroundImage = "url('https://source.unsplash.com/1600x900/?sunny')";
+      break;
+    case "clouds":
+      backgroundImage = "url('https://source.unsplash.com/1600x900/?cloudy')";
+      break;
+    case "rain":
+      backgroundImage = "url('https://source.unsplash.com/1600x900/?rain')";
+      break;
+    case "snow":
+      backgroundImage = "url('https://source.unsplash.com/1600x900/?snow')";
+      break;
+    case "thunderstorm":
+      backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?thunderstorm')";
+      break;
+    default:
+      backgroundImage = "url('https://source.unsplash.com/1600x900/?weather')";
+      break;
+  }
+
+  document.body.style.backgroundImage = backgroundImage;
+  document.body.style.backgroundSize = "cover";
+}
